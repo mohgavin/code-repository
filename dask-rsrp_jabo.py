e343689ec19fc065231d12d5b3163dba29aeb20d
mohgavin <mohammad.gavin@gmail.com>
2023-05-29 07:01:21 +0700

    On branch main Your branch is up to date with 'main/main'.
    Changes to be committed:
    	modified:   CR_consolidate_to_one_file.ipynb
    	modified:   bigquery_query_CR.ipynb
    	modified:   boundary_plot_20230705.py
    	modified:   cr-google-query1.ipynb
    	new file:   dask-rsrp_jabo.py
    	modified:   dask_inside and intersect polygon.ipynb
    	modified:   directory_output_8007.ipynb
    	modified:   directory_output_8008.ipynb
    	modified:   directory_output_enm3.ipynb
    	modified:   directory_output_enm5.ipynb
    	modified:   extract MDT & rename according to the name of zip files.ipynb
    	modified:   mdt_consolidate_to_onefolder and rename it.ipynb
    	modified:   plotly_test.ipynb
    	modified:   plotting inside boundary.ipynb
    	modified:   plotting to the boundary.ipynb
    	modified:   plotting to the grid.ipynb
    	modified:   poi_cellrebel_rsrp&capacity.ipynb
    	modified:   rsrp_grid plotting edit.ipynb
    	modified:   rsrp_grid plotting.ipynb
    	modified:   sandbox2.ipynb
    	modified:   server-test.py
    	modified:   sites plotting near and inside the grid.ipynb
    	modified:   sites_plotting_near distance.ipynb
    	deleted:    test.py
    	modified:   throughput_grid_plotting.ipynb

---
+++ dask-rsrp_jabo.py
@@ -0,0 +1,34 @@
+#!/home/nivag/2023-Linux/.python-3.10/bin/python
+
+import dask.dataframe as dask_pd
+import dask_geopandas as dask_gpd
+import geopandas as gpd
+from dask.distributed import Client
+
+if __name__ == '__main__':
+        client = Client(n_workers=1, threads_per_worker=4, processes=True, memory_target_fraction=0.55, memory_limit='6GB')
+
+        dask_df_mdt =  dask_pd.read_csv('Compile-MDT/mdt*.csv', usecols=[0,1,2,3,4,6,7,8,9], assume_missing=True)
+        dask_df_mdt['rsrp'] = 'rsrp'
+        grid = dask_pd.read_parquet('Grid Folder/100x100_gridjabo.parquet')
+
+        grid['geometry_polygon'] = grid['geometry'].astype(str)
+        dask_df_mdt['pointer'] = "POINT (" + dask_df_mdt['longitude'].astype(str) + " " + dask_df_mdt['latitude'].astype(str) + ")"
+
+        dask_gdf_mdt = dask_gpd.from_dask_dataframe(dask_df_mdt, geometry=dask_df_mdt["pointer"].map_partitions(
+                gpd.GeoSeries.from_wkt, meta=gpd.GeoSeries([])),)
+        #dask_gdf_mdt = dask_gdf_mdt.set_crs(4326)
+
+        dask_gdf_grid = dask_gpd.from_dask_dataframe(grid, geometry=grid['geometry_polygon'].map_partitions(
+                gpd.GeoSeries.from_wkt, meta=gpd.GeoSeries([])),)
+        #dask_gdf_grid = dask_gdf_grid.set_crs(4326)
+
+        dask_gdf_mdt = dask_gdf_mdt.sjoin(dask_gdf_grid, how='inner', predicate='within')
+        dask_gdf_mdt = dask_gdf_mdt.drop(columns=['index_right', 'id', 'pointer'])
+
+        dask_gdf_mdt['rsrp'] = dask_gdf_mdt['rsrp'].astype('category')
+        dask_gdf_mdt['rsrp'] = dask_gdf_mdt['rsrp'].cat.as_known()
+        pivot = dask_gdf_mdt.pivot_table(index='geometry_polygon', columns='rsrp', values='rsrp_serving', aggfunc='mean')
+
+        pivot = pivot.compute()
+        pivot.to_parquet('test.parquet')
--- server-test.py
+++ server-test.py
@@ -1,3 +1,5 @@
+#!/home/nivag/2023 - Linux/.python-3.10-1/bin/python
+
 import subprocess
 import pexpect
 import sys
