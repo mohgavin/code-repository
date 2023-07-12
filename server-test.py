89d19b4c45a83b60737755ec244a351a910f4f08
mohgavin <mohammad.gavin@gmail.com>
2023-07-12 11:14:47 +0700

    Changes to be committed: 	modified:   CR_consolidate_to_one_file.ipynb 	modified:   bigquery_query_CR.ipynb 	new file:   csv_polygon/mangkuluhur.csv 	new file:   csv_polygon/mangkuluhur.qmd 	deleted:    dask_inside and intersect polygon.ipynb 	new file:   directory_output_8007-mdt-polygon.ipynb 	modified:   directory_output_8007-mdt.ipynb 	new file:   directory_output_8008-mdt-polygon.ipynb 	modified:   directory_output_8008-mdt.ipynb 	modified:   l1800-mdt-rsrp-bad-grid-process.ipynb 	modified:   l2100-mdt-rsrp-bad-grid-process.ipynb 	modified:   l900-mdt-rsrp-bad-grid-process.ipynb 	new file:   parsing-combined-rsrp-cell-from_dask.ipynb 	modified:   parsing-l900-rsrp-cell-from_dask.ipynb 	deleted:    server-test.py
    Changes not staged for commit:
    	modified:   dask-rsrp-l1800_polygon.py
    	modified:   dask-rsrp-l900-40x40grid_jabo.py
    	modified:   dask-rsrp-l900_polygon.py
    	modified:   dask-throughput-l1800-40x40grid_jabo.py
    	modified:   dask-throughput-l2100-40x40grid_jabo.py
    	modified:   dask-throughput-l900-40x40grid_jabo.py
    
    Untracked files:
    	1.RSRP-polygon_MDT_Folder_Consolidate_Rename.py
    	2.RSRP-polygon_MDT_ExtractFiles-DeleteZip.py
    	consolidate-to-one-file.py
    	cr-rsrp-process_xlarea.py
    	dask-process-xlarea.ipynb
    	dask-process_CR_XLArea.py
    	dask-rsrp-combined-polygon.py
    	dask-rsrp-l1800_badgrid.py
    	dask-rsrp-l2100_badgrid.py
    	dask-rsrp-l900_badgrid.py
    	dask-throughput-train_jabo.py
    	shell-rsrp-40x40grid.py

--- server-test.py
+++
@@ -1,9 +0,0 @@
-#!/home/nivag/2023 - Linux/.python-3.10-1/bin/python
-
-import subprocess
-import pexpect
-import sys
-import time
-             
-child = pexpect.spawn('sudo mkdir test', timeout=30)
-print(child.read())
