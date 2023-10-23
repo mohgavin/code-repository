32a757f8ba0b3566c80eac75658f5ffaee23b0cf
mohgavin <mohammad.gavin@gmail.com>
2023-10-23 14:33:59 +0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   README.md
    	new file:   big_Data_Scripts/pyspark-rsrp-combined-40x40grid_jabo.py
    	modified:   create-linestring-to-nearest.ipynb
    	modified:   csv_polygon/Underground MRT Route Senayan-Bundaran HI.csv
    	modified:   database_process/Windows Script/hardware.py
    	modified:   database_process/Windows Script/main.exe
    	modified:   database_process/Windows Script/main.py
    	modified:   database_process/Windows Script/main.spec
    	modified:   database_process/Windows Script/upx.exe
    	modified:   database_process/connectivity.py
    	modified:   database_process/hardware.py
    	modified:   database_process/main.py
    	modified:   database_process/requirements.txt
    	modified:   database_process/tower.py
    	modified:   excel/prophet-w35-delta.xlsx
    	modified:   forecast/Arima-Auto-Correlation.ipynb
    	new file:   grid_folder/40x40grid_alljabo_filtered.parquet
    	new file:   grid_folder/40x40grid_yogyasleman.csv
    	deleted:    grid_folder/grid_folder.7z
    	modified:   paper/ts_137320v160200p_Minimization of Drive Test.pdf
    	modified:   picture/AB Testing - ARIMA vs Prophet.png
    	modified:   picture/GBK - Throughput Power.png
    	modified:   picture/Histogram - Week35 Delta Forecast with Actual.png
    	modified:   picture/ISD - LineString.png
    	modified:   picture/Jaksel - Population Map.png
    	modified:   picture/Jaksel - Signal Power.png
    	new file:   picture/Pomelotel - 5x5 Grid.png
    	modified:   picture/getting multiple nearest sites.png
    	modified:   picture/inbuilding - polygon.png
    	modified:   picture/ts_137320v160200p.pdf:Zone.Identifier
    	modified:   polygon/Commuter_combined_RevA.DAT
    	modified:   polygon/Commuter_combined_RevA.ID
    	modified:   polygon/Commuter_combined_RevA.IND
    	modified:   polygon/Commuter_combined_RevA.MAP
    	modified:   polygon/Commuter_combined_RevA.TAB
    	modified:   powerbI/AB Testing - Arima vs Prophet.pbix
    	modified:   powerbI/personal_project_gavin.pbix
    	modified:   ppt/Bad Grid Identification with Multi Process and Parallelization - Dask.pptx
    	modified:   ppt/Bad Grid Identification with Multi Process and Parallelization - Dask.pptx:Zone.Identifier
    	modified:   ppt/Define Implementation Scenario - Decision Tree & Random Forest.pptx
    	modified:   ppt/Define Implementation Scenario - Decision Tree & Random Forest.pptx:Zone.Identifier
    	modified:   query/directory_output_8007-mdt-polygon.ipynb
    	modified:   query/directory_output_8007-mdt.ipynb
    	modified:   query/directory_output_8007-throughput-polygon.ipynb
    	modified:   query/directory_output_8007-throughput.ipynb
    	modified:   query/directory_output_8008-mdt-polygon.ipynb
    	modified:   query/directory_output_8008-mdt.ipynb
    	modified:   query/directory_output_8008-throughput-polygon.ipynb
    	modified:   query/directory_output_8008-throughput.ipynb
    	modified:   query/directory_output_8009-mdt-polygon.ipynb
    	modified:   query/directory_output_8009-mdt.ipynb
    	modified:   query/directory_output_8009-throughput-polygon.ipynb
    	modified:   query/directory_output_8009-throughput.ipynb
    	modified:   requirements.txt
    	modified:   sandbox-2.ipynb
    	modified:   sandbox-3.ipynb
    	new file:   sandbox-4.ipynb
    
    Changes not staged for commit:
    	modified:   .gitignore
    	deleted:    picture/Pomelotel - 5x5 Grid.png
    	deleted:    picture/getting multiple nearest sites.png
    	deleted:    picture/ts_137320v160200p.pdf:Zone.Identifier

---
+++ big_Data_Scripts/pyspark-rsrp-combined-40x40grid_jabo.py
@@ -0,0 +1,48 @@
+#!/home/nivag/.env/bin/python
+
+# by @mohgavin
+from pyspark.sql import SparkSession
+from sedona.register import SedonaRegistrator
+from sedona.utils import SedonaKryoRegistrator, SedonaConf
+from pyspark.sql.functions import col, concat_ws
+from pyspark.sql.types import StringType
+
+if __name__ == '__main__':
+    # Initialize Spark session
+    spark = SparkSession.builder.appName("SedonaExample").getOrCreate()
+
+    # Register Sedona
+    SedonaRegistrator.registerAll(spark)
+    SedonaKryoRegistrator.registerKryoClasses(spark)
+
+    # Set Sedona configuration
+    SedonaConf.addVizServerExecutors(spark._sc)
+    SedonaConf.addVizServerDependency(spark._sc)
+
+    print('Loading Files...')
+    mdt_df = spark.read.csv('Compile-MDT/mdt*.csv', header=True, inferSchema=True)
+    grid_df = spark.read.parquet('grid_folder/40x40grid_alljabo_filtered.parquet')
+
+    # Create Sedona geometries
+    mdt_df = mdt_df.withColumn("pointer", concat_ws(" ", col("longitude").cast(StringType()), col("latitude").cast(StringType())))
+    mdt_df.createOrReplaceTempView("mdt_df")
+    spark.sql("SELECT *, ST_PointFromText(pointer, ' ') AS geom FROM mdt_df").createOrReplaceTempView("mdt_df")
+
+    grid_df = grid_df.withColumn("geometry_polygon", col("geometry").cast(StringType()))
+    grid_df.createOrReplaceTempView("grid_df")
+    spark.sql("SELECT *, ST_GeomFromWKT(geometry_polygon) AS geom FROM grid_df").createOrReplaceTempView("grid_df")
+
+    print('Join Operation...')
+    spark.sql("SELECT m.*, g.* FROM mdt_df AS m, grid_df AS g WHERE ST_Intersects(m.geom, g.geom)").createOrReplaceTempView("joined_df")
+
+    # Perform the necessary operations to create pivot tables
+    pivot_mean = spark.sql("SELECT geometry_polygon, rsrp_combined, mean(rsrp_serving) AS mean_rsrp_serving FROM joined_df GROUP BY geometry_polygon, rsrp_combined")
+    pivot_count = spark.sql("SELECT geometry_polygon, rsrp_combined, count(rsrp_serving) AS count_rsrp_serving FROM joined_df GROUP BY geometry_polygon, rsrp_combined")
+
+    print('Saving Files...')
+    pivot_mean.coalesce(1).write.csv('result/rsrp-combined-alljabo-40x40.csv', header=True)
+    pivot_count.coalesce(1).write.csv('result/rsrp-combined-alljabo-40x40-pop.csv', header=True)
+
+    print('Finished...')
+
+    spark.stop()
